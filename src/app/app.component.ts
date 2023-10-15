d0405b65eb0166e987bcc64b0ccadea721467c57
HonestDaniel <killedsparrow@gmail.com>
2023-10-15 19:22:55 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
     }
   }
   fetchData(requestInvoked: boolean) {
-    let cancelRequest = new Subject<void>();
+    const cancelRequest = new Subject<void>();
     this.http.get<number>(
       'https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new'
     )
